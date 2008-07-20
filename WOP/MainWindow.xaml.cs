783d1e07f66d99689cf2dc38aa71bb709bdfaa19
basti <basti@1c8fc246-6724-0410-aa23-c3c678790691>
2008-07-20 09:06:22 +0000

    -more code -startet perf testing im, fi...

--- WOP/MainWindow.xaml.cs
+++ WOP/MainWindow.xaml.cs
@@ -20,51 +20,45 @@ namespace WOP
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BackgroundWorker bgSplasher = new BackgroundWorker();
-        private WOPSplash wops = new WOPSplash();
+        private BackgroundWorker bgSplasher = new BackgroundWorker ();
+        private WOPSplash wops = new WOPSplash ();
         private int til = 50;
 
-        public MainWindow()
-        {
-            InitializeComponent();
+        public MainWindow () {
+            this.Visibility = Visibility.Hidden;
+            InitializeComponent ();
             bgSplasher.WorkerReportsProgress = true;
-            bgSplasher.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgSplasher_RunWorkerCompleted);
-            bgSplasher.DoWork += new DoWorkEventHandler(bgSplasher_DoWork);
-            bgSplasher.ProgressChanged += new ProgressChangedEventHandler(bgSplasher_ProgressChanged);
+            bgSplasher.RunWorkerCompleted += new RunWorkerCompletedEventHandler ( bgSplasher_RunWorkerCompleted );
+            bgSplasher.DoWork += new DoWorkEventHandler ( bgSplasher_DoWork );
+            bgSplasher.ProgressChanged += new ProgressChangedEventHandler ( bgSplasher_ProgressChanged );
             wops.Opacity = 0;
-            wops.Show();
-            bgSplasher.RunWorkerAsync();
+            wops.Show ();
+            bgSplasher.RunWorkerAsync ();
         }
 
-        void bgSplasher_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
+        void bgSplasher_ProgressChanged ( object sender, ProgressChangedEventArgs e ) {
             wops.Opacity = 1f / til * e.ProgressPercentage;
         }
 
-        void bgSplasher_DoWork(object sender, DoWorkEventArgs e)
-        {
-            for (int i = 0; i < til; i++)
-            {
-                bgSplasher.ReportProgress(i);
-                Thread.Sleep(10);
+        void bgSplasher_DoWork ( object sender, DoWorkEventArgs e ) {
+            for (int i = 0; i < til; i++) {
+                bgSplasher.ReportProgress ( i );
+                Thread.Sleep ( 10 );
             }
-            Thread.Sleep(1000);
-            for (int i = til/2; i >=0; i--)
-            {
-                bgSplasher.ReportProgress(i*2);
-                Thread.Sleep(1);
+            Thread.Sleep ( 1000 );
+            for (int i = til / 2; i >= 0; i--) {
+                bgSplasher.ReportProgress ( i * 2 );
+                Thread.Sleep ( 1 );
             }
         }
 
-        void bgSplasher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            wops.Close();
+        void bgSplasher_RunWorkerCompleted ( object sender, RunWorkerCompletedEventArgs e ) {
+            wops.Close ();
             this.Visibility = Visibility.Visible;
         }
 
-        private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
-        {
-            
+        private void Window_Loaded ( object sender, System.Windows.RoutedEventArgs e ) {
+
         }
 
     }
---
+++ WOP/Tests/IMTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using NUnit.Framework;
+using WOP.Objects;
+
+namespace WOP.Tests
+{
+    [TestFixture]
+    public class IMTest
+    {
+        [Test]
+        public void TestIMSpeed()
+        {
+            DateTime start = DateTime.Now;
+            foreach (string s in Directory.GetFiles(@"..\..\..\IM\pix", "*jpg"))
+            {
+                var fin = new FileInfo(s);
+                var fout = new FileInfo(Path.Combine(fin.DirectoryName, fin.Name + "_small" + fin.Extension));
+                ImageWorker.ShrinkImageFI(fin, fout, new Size(400, 400));
+            }
+            TimeSpan ts = DateTime.Now.Subtract(start);
+            string d = Directory.GetCurrentDirectory();
+            Console.WriteLine(ts);
+        }
+    }
+}
