af0d62e8e63a1566ebe8ce903af611d6ec329288
basti <basti@1c8fc246-6724-0410-aa23-c3c678790691>
2009-02-16 13:10:54 +0000

    -made themes switchable (45min)

--- WOP/App.xaml.cs
+++ WOP/App.xaml.cs
@@ -1,15 +1,44 @@
-﻿using System.Windows;
-using WOP.Tasks;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Xml;
 
 namespace WOP {
     /// <summary>
     /// Interaktionslogik für "App.xaml"
     /// </summary>
     public partial class App : Application {
-        private void AppStartup(object sender, StartupEventArgs args)
-        {
-            Job j = Job.CreateTestJob();
-            j.Start();
-        }
+
+      public static App MainApp;
+      public static ResourceDictionary blackTheme;
+      public static ResourceDictionary whiteTheme;
+
+      private void Application_Startup(object sender, StartupEventArgs e)
+      {
+        string blackThemeStr = File.ReadAllText("Styles/BlackTheme.xaml");
+        StringReader sr = new StringReader(blackThemeStr);
+        XmlReader xr = XmlReader.Create(sr);
+        blackTheme = (ResourceDictionary)XamlReader.Load(xr);
+        string whiteThemeStr = File.ReadAllText("Styles/WhiteTheme.xaml");
+        sr = new StringReader(whiteThemeStr);
+        xr = XmlReader.Create(sr);
+        whiteTheme = (ResourceDictionary)XamlReader.Load(xr);
+        ActivateBlackTheme();
+        MainApp = this;
+      }
+
+      public void ActivateBlackTheme()
+      {
+        Application.Current.Resources.MergedDictionaries.Remove(whiteTheme);
+        Application.Current.Resources.MergedDictionaries.Remove(blackTheme);
+        Application.Current.Resources.MergedDictionaries.Add(blackTheme);
+      }
+
+      public void ActivateWhiteTheme()
+      {
+        Application.Current.Resources.MergedDictionaries.Remove(whiteTheme);
+        Application.Current.Resources.MergedDictionaries.Remove(blackTheme);
+        Application.Current.Resources.MergedDictionaries.Add(whiteTheme);
+      }
     }
 }
--- WOP/MainWindow.xaml.cs
+++ WOP/MainWindow.xaml.cs
@@ -4,104 +4,116 @@ using System.Threading;
 using System.Windows;
 using WOP.Tasks;
 
-namespace WOP {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        private readonly BackgroundWorker bgSplasher = new BackgroundWorker();
-        private readonly WOPSplash wops = new WOPSplash();
-        private Job theJob;
-        private int til = 50;
+namespace WOP
+{
+  /// <summary>
+  /// Interaktionslogik für MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private readonly BackgroundWorker bgSplasher = new BackgroundWorker();
+    private readonly WOPSplash wops = new WOPSplash();
+    private Job theJob;
+    private int til = 50;
 
-        public MainWindow()
-        {
-            Visibility = Visibility.Hidden;
-            InitializeComponent();
-            bgSplasher.WorkerReportsProgress = true;
-            bgSplasher.RunWorkerCompleted += bgSplasher_RunWorkerCompleted;
-            bgSplasher.DoWork += bgSplasher_DoWork;
-            bgSplasher.ProgressChanged += bgSplasher_ProgressChanged;
-            wops.Opacity = 0;
-            wops.Show();
-            bgSplasher.RunWorkerAsync();
-        }
-
-        private void bgSplasher_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            wops.Opacity = 1f/til*e.ProgressPercentage;
-        }
-
-        private void bgSplasher_DoWork(object sender, DoWorkEventArgs e)
-        {
-            bool withSplash = false;
-            if (withSplash) {
-                for (int i = 0; i < til; i++) {
-                    bgSplasher.ReportProgress(i);
-                    Thread.Sleep(10);
-                }
-                Thread.Sleep(2000);
-                for (int i = til/2; i >= 0; i--) {
-                    bgSplasher.ReportProgress(i*2);
-                    Thread.Sleep(1);
-                }
-            }
-        }
-
-        private void bgSplasher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            wops.Close();
-            Visibility = Visibility.Visible;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            createDefaultJob();
-            addJobUIs();
-        }
-
-        private void createDefaultJob()
-        {
-            // configure tasks
-            theJob = new Job();
-            theJob.Name = "my first job";
-            theJob.AddTask(new FileGatherTask { IsEnabled = true, DeleteSource = false, FilePattern = "*.jpg", RecurseDirectories = true, SourceDirectory = @"..\..\..\testdata\pixrotate", TargetDirectory = @"c:\tmp" });
-            theJob.AddTask(new FileRenamerTask {IsEnabled = true, RenamePattern = "bastitest_{0}"});
-            theJob.AddTask(new ImageShrinkTask {IsEnabled = true, SizeX = 400, SizeY = 400, PreserveOriginals = true, NameExtension = "_thumb"});
-            theJob.AddTask(new ImageRotateTask {IsEnabled = true});
-            //theJob.AddTask(new FTPTask() { IsEnabled = true, Server = "www.dotob.de", ServerDirectory = "files", UserName = "web1", Password = "celeron" });
-            //snootheJob.AddTask(new GEOTagTask { IsEnabled = false });
-        }
-
-        private void addJobUIs()
-        {
-            foreach (ITask task in theJob.TasksList) {
-                if (task.UI != null) {
-                    task.UI.Margin = new Thickness(2);
-                    m_sp_tasks.Children.Add(task.UI);
-                }
-            }
-        }
-
-        private void jj_JobFinished(object sender, EventArgs e)
-        {
-            MessageBox.Show("job finished");
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            m_lb_jobs.Items.Add(theJob);
-            createDefaultJob();
-        }
-
-        private void m_mn_whitetheme_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void m_mn_blacktheme_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
+    public MainWindow()
+    {
+      this.Visibility = Visibility.Hidden;
+      this.InitializeComponent();
+      this.bgSplasher.WorkerReportsProgress = true;
+      this.bgSplasher.RunWorkerCompleted += this.bgSplasher_RunWorkerCompleted;
+      this.bgSplasher.DoWork += this.bgSplasher_DoWork;
+      this.bgSplasher.ProgressChanged += this.bgSplasher_ProgressChanged;
+      this.wops.Opacity = 0;
+      this.wops.Show();
+      this.bgSplasher.RunWorkerAsync();
     }
+
+    private void bgSplasher_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      this.wops.Opacity = 1f / this.til * e.ProgressPercentage;
+    }
+
+    private void bgSplasher_DoWork(object sender, DoWorkEventArgs e)
+    {
+      bool withSplash = false;
+      if (withSplash) {
+        for (int i = 0; i < this.til; i++) {
+          this.bgSplasher.ReportProgress(i);
+          Thread.Sleep(10);
+        }
+        Thread.Sleep(2000);
+        for (int i = this.til / 2; i >= 0; i--) {
+          this.bgSplasher.ReportProgress(i * 2);
+          Thread.Sleep(1);
+        }
+      }
+    }
+
+    private void bgSplasher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      this.wops.Close();
+      this.Visibility = Visibility.Visible;
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      this.createDefaultJob();
+      this.addJobUIs();
+    }
+
+    private void createDefaultJob()
+    {
+      // configure tasks
+      this.theJob = new Job();
+      this.theJob.Name = "my first job";
+      this.theJob.AddTask(new FileGatherTask {IsEnabled = true, DeleteSource = false, FilePattern = "*.jpg", RecurseDirectories = true, SourceDirectory = @"..\..\..\testdata\pixrotate", TargetDirectory = @"c:\tmp"});
+      this.theJob.AddTask(new FileRenamerTask {IsEnabled = true, RenamePattern = "bastitest_{0}"});
+      this.theJob.AddTask(new ImageShrinkTask {IsEnabled = true, SizeX = 400, SizeY = 400, PreserveOriginals = true, NameExtension = "_thumb"});
+      this.theJob.AddTask(new ImageRotateTask {IsEnabled = true});
+      //theJob.AddTask(new FTPTask() { IsEnabled = true, Server = "www.dotob.de", ServerDirectory = "files", UserName = "web1", Password = "celeron" });
+      //snootheJob.AddTask(new GEOTagTask { IsEnabled = false });
+    }
+
+    private void addJobUIs()
+    {
+      foreach (ITask task in this.theJob.TasksList) {
+        if (task.UI != null) {
+          task.UI.Margin = new Thickness(2);
+          this.m_sp_tasks.Children.Add(task.UI);
+        }
+      }
+    }
+
+    private void jj_JobFinished(object sender, EventArgs e)
+    {
+      MessageBox.Show("job finished");
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      this.m_lb_jobs.Items.Add(this.theJob);
+      this.createDefaultJob();
+    }
+
+    private void m_mn_whitetheme_Checked(object sender, RoutedEventArgs e)
+    {
+      if (this.m_mn_whitetheme != null && this.m_mn_blacktheme != null) {
+        if (this.m_mn_whitetheme.IsChecked) {
+          App.MainApp.ActivateWhiteTheme();
+        }
+        this.m_mn_blacktheme.IsChecked = false;
+      }
+    }
+
+    private void m_mn_blacktheme_Checked(object sender, RoutedEventArgs e)
+    {
+      if (this.m_mn_whitetheme != null && this.m_mn_blacktheme != null) {
+        if (this.m_mn_blacktheme.IsChecked) {
+          App.MainApp.ActivateBlackTheme();
+        }
+        this.m_mn_whitetheme.IsChecked = false;
+      }
+    }
+  }
 }
