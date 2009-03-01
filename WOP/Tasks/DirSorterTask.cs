05cc4bbed4e3ed993f585497a72476d734a8cfaf
basti <basti@1c8fc246-6724-0410-aa23-c3c678790691>
2009-03-01 14:55:38 +0000

    -fix #2:introduce dirsorter task, not ready yet -reformat xaml files

---
+++ WOP/Tasks/DirSorterTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WOP.Objects;
+using WOP.TasksUI;
+
+namespace WOP.Tasks
+{
+  public class DirSorterTask:SkeletonTask
+  {
+    public DirSorterTask()
+    {
+      this.Name = "In Ordner verteilen";
+      this.UI = new DirSorterTaskUI();
+      this.UI.DataContext = this;
+    }
+
+    public override ITask CloneNonDynamicStuff()
+    {
+      DirSorterTask t = new DirSorterTask();
+      t.IsEnabled = this.IsEnabled;
+      return t;
+    }
+
+    public override bool Process(ImageWI iwi)
+    {
+      return true;
+    }
+  }
+}
